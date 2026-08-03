2d0436be9826ccaa9d9e551e10cbb3456880541c
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-03 23:02:38 +0800

    added initial stuff in player class

--- Player.java
+++ Player.java
@@ -1,3 +1,16 @@
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Player {
+    private String name;
+    private List<Card> hand;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
 }
