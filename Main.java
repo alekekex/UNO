adc4329a5cface894e9ecf3eb221c2e750c388e1
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-05 01:37:05 +0800

    added initial game setup

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean isRunning = true;
 
+        while (isRunning) {
+            System.out.println("Welcome to UNO!");
+            System.out.println("1) Play Game");
+            System.out.println("2) Exit Program");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    UNO game = new UNO();
+                    // player initialization and game start
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("Invalid option! Try again.");
+            }
+        }
+
+        sc.close();
     }
 }
--- UNO.java
+++ UNO.java
@@ -1,3 +1,18 @@
-public class UNO {
+import java.util.ArrayList;
+import java.util.List;
 
+public class UNO {
+    private Deck deck;
+    private List<Card> pile;
+    private List<Player> players;
+    private int playerIdx;
+    private boolean isGameOver;
+
+    public UNO() {
+        this.deck = new Deck();
+        this.pile = new ArrayList<>();
+        this.players = new ArrayList<>();
+        this.playerIdx = 0;
+        this.isGameOver = false;
+    }
 }
