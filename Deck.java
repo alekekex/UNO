86bdfd135f87c2b61a32c6b5a5adfdc56852516a
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-04 17:22:56 +0800

    added initialization of deck in deck class

--- Deck.java
+++ Deck.java
@@ -7,6 +7,7 @@ public class Deck {
 
     public Deck() {
         this.deck = new ArrayList<>();
+        initializeDeck();
     }
 
     public Card drawCard() {
@@ -22,6 +23,42 @@ public class Deck {
 
     public boolean isDeckEmpty() {
         return deck.isEmpty();
+    }
+
+    public void initializeDeck() {
+        deck.add(new Card("NORMAL", "0", "RED"));
+        deck.add(new Card("NORMAL", "0", "GREEN"));
+        deck.add(new Card("NORMAL", "0", "YELLOW"));
+        deck.add(new Card("NORMAL", "0", "BLUE"));
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 9; j++) {
+                deck.add(new Card("NORMAL", String.valueOf(j + 1), "RED"));
+                deck.add(new Card("NORMAL", String.valueOf(j + 1), "GREEN"));
+                deck.add(new Card("NORMAL", String.valueOf(j + 1), "YELLOW"));
+                deck.add(new Card("NORMAL", String.valueOf(j + 1), "BLUE"));
+            }
+
+            deck.add(new Card("ACTION", "S", "RED"));
+            deck.add(new Card("ACTION", "S", "GREEN"));
+            deck.add(new Card("ACTION", "S", "YELLOW"));
+            deck.add(new Card("ACTION", "S", "BLUE"));
+
+            deck.add(new Card("ACTION", "R", "RED"));
+            deck.add(new Card("ACTION", "R", "GREEN"));
+            deck.add(new Card("ACTION", "R", "YELLOW"));
+            deck.add(new Card("ACTION", "R", "BLUE"));
+
+            deck.add(new Card("ACTION", "+2", "RED"));
+            deck.add(new Card("ACTION", "+2", "GREEN"));
+            deck.add(new Card("ACTION", "+2", "YELLOW"));
+            deck.add(new Card("ACTION", "+2", "BLUE"));
+        }
+
+        for (int i = 0; i < 4; i++) {
+            deck.add(new Card("WILD", "CC", "GRAY"));
+            deck.add(new Card("WILD", "+4", "GRAY"));
+        }
     }
 
     public void shuffleDeck() {
@@ -41,8 +78,7 @@ public class Deck {
             System.out.println("[Empty]");
         else {
             for (Card card : deck)
-                System.out.print(card.getCard() + " ");
-            System.out.println();
+                System.out.println(card.getCard());
         }
     }
 }
