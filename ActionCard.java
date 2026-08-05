b34cbc454bd027c7e8999f4fe649de5c2c90d3c8
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-06 00:57:55 +0800

    added effect implementation methods

--- ActionCard.java
+++ ActionCard.java
@@ -8,12 +8,15 @@ public class ActionCard extends Card {
         switch (getValue()) {
             case "S":
                 System.out.println("Effect: Skipped next turn!"); // temp
+                game.skipNextPlayer();
                 break;
             case "R":
                 System.out.println("Effect: Reversed turn order!"); // temp
+                game.reverseDirection();
                 break;
             case "+2":
                 System.out.println("Effect: Added 2 cards to next player!"); // temp
+                game.drawTwo();
                 break;
         }
     }
--- WildCard.java
+++ WildCard.java
@@ -1,16 +1,29 @@
 public class WildCard extends Card {
+    String activeColor;
+
     public WildCard(String value) {
         super(value, "GRAY");
+    }
+
+    public void setActiveColor(String activeColor) {
+        this.activeColor = activeColor;
+    }
+
+    @Override
+    public String getColor() {
+        return activeColor;
     }
 
     @Override
     public void applyCardEffect(UNO game) {
         switch (getValue()) {
             case "CC":
-                System.out.println("Effect: Changed Color!"); // temp
+                System.out.println("Effect: Changed color!"); // temp
+                game.changeColor(this);
                 break;
             case "+4":
                 System.out.println("Effect: Added 4 cards to next player and changed color!"); // temp
+                game.drawFour(this);
                 break;
         }
     }
