855798f4957e13bffc36ab3312873b999e2a9a20
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-06 21:30:27 +0800

    refactored game method to sub methods

--- UNO.java
+++ UNO.java
@@ -24,137 +24,170 @@ public class UNO {
     }
 
     public void playGame() {
+        dealCards();
+        displayStartingCard();
+
+        while (!isGameOver) {
+            Player currPlayer = players.get(playerIdx);
+
+            displayPlayerHand(currPlayer);
+            playTurn(currPlayer);
+
+            checkIfUno(currPlayer);
+            checkIfGameOver(currPlayer);
+
+            if (!isGameOver)
+                advanceTurn();
+        }
+
+        displayWinner();
+    }
+
+    public void dealCards() {
         for (int i = 0; i < 7; i++) {
             for (Player player : players) {
                 Card card = deck.drawCard();
                 player.receiveCard(card);
             }
         }
+    }
 
-        boolean isValid = false;
+    public void displayStartingCard() {
+        Card card = deck.drawCard();
 
-        while (!isValid) {
-            Card startCard = deck.drawCard();
-
-            if (startCard instanceof NormalCard) {
-                isValid = true;
-                pile.add(startCard);
-            } else deck.addCardToBottom(startCard);
+        while (!(card instanceof NormalCard)) {
+            deck.addCardToBottom(card);
+            card = deck.drawCard();
         }
 
-        System.out.println("Starting Card: " + pile.get(pile.size() - 1).getCard());
+        pile.add(card);
+
+        System.out.println("Starting Card: " + card.getCard());
+        System.out.println();
+    }
+
+    public void displayPlayerHand(Player player) {
+        System.out.println(player.getName() + "\'s hand:");
+        player.showHand();
+        System.out.println();
+    }
+
+    public Card getTopCard() {
+        return pile.get(pile.size() - 1);
+    }
+
+    public void playTurn(Player player) {
+        boolean isTurnOver = false;
+
+        while (!isTurnOver) {
+            System.out.println(player.getName() + ", what is your move?");
+            System.out.println("1) Play Card");
+            System.out.println("2) Draw Card");
+            int choice = Input.getIntInput("Enter your choice: ", 1, 2);
+
+            switch (choice) {
+                case 1:
+                    isTurnOver = handlePlayCard(player);
+                    break;
+                case 2:
+                    isTurnOver = handleDrawCard(player);
+                    break;
+            }
+        }
+    }
+
+    public boolean handlePlayCard(Player player) {
+        int cardIdx = Input.getIntInput("Select card from hand: ", 1, player.getHandSize());
+        Card card = player.getCard(cardIdx);
+        boolean isOver = false;
+
+        if (card.canPlayOn(getTopCard())) {
+            isOver = true;
+            player.playCard(cardIdx);
+            pile.add(card);
+
+            System.out.println();
+            displayTopCard();
+
+            card.applyCardEffect(this);
+        } else System.out.println("Invalid option! Card is not valid.");
+
+        System.out.println();
+        return isOver;
+    }
+
+    public boolean handleDrawCard(Player player) {
+        Card card = deck.drawCard();
+        player.receiveCard(card);
+
+        System.out.println();
+        System.out.println("Drawn Card: " + card.getCard());
         System.out.println();
 
-        while (!isGameOver) {
-            Player currPlayer = players.get(playerIdx);
+        System.out.println(player.getName() + ", what do you want to do?");
+        System.out.println("1) Play Drawn Card");
+        System.out.println("2) Keep Drawn Card");
+        int choice = Input.getIntInput("Enter your choice: ", 1, 2);
 
-            System.out.println(currPlayer.getName() + "\'s hand:");
-            currPlayer.showHand();
-            System.out.println();
-
-            boolean isTurnOver = false;
-
-            while (!isTurnOver) {
-                System.out.println(currPlayer.getName() + ", what is your move?");
-                System.out.println("1) Play Card");
-                System.out.println("2) Draw Card");
-                int choice = Input.getIntInput("Enter your choice: ", 1, 2);
-
-                switch (choice) {
-                    case 1:
-                        int cardIdx = Input.getIntInput("Select card from hand: ",
-                                1, currPlayer.getHandSize());
-                        Card chosenCard = currPlayer.getCard(cardIdx);
-
-                        if (chosenCard.canPlayOn(pile.get(pile.size() - 1))) {
-                            isTurnOver = true;
-                            currPlayer.playCard(cardIdx);
-                            pile.add(chosenCard);
-
-                            System.out.println();
-                            System.out.println("Top Card: " + pile.get(pile.size() - 1).getCard());
-
-                            if (pile.get(pile.size() - 1) instanceof WildCard &&
-                                    !pile.get(pile.size() - 1).getColor().equals("NONE"))
-                                System.out.println("Active Color: " +
-                                        ((WildCard) pile.get(pile.size() - 1)).getActiveColor());
-
-                            chosenCard.applyCardEffect(this);
-                        } else System.out.println("Invalid option! Card is not valid.");
-
-                        System.out.println();
-                        break;
-                    case 2:
-                        Card drawnCard = deck.drawCard();
-                        currPlayer.receiveCard(drawnCard);
-
-                        System.out.println();
-                        System.out.println("Drawn Card: " + drawnCard.getCard());
-                        System.out.println();
-
-                        System.out.println(currPlayer.getName() + ", what do you want to do?");
-                        System.out.println("1) Play Drawn Card");
-                        System.out.println("2) Skip Turn");
-                        int choice2 = Input.getIntInput("Enter your choice: ", 1, 2);
-
-                        switch (choice2) {
-                            case 1:
-                                Card chosenDrawnCard = currPlayer.getCard(currPlayer.getHandSize());
-
-                                if (chosenDrawnCard.canPlayOn(pile.get(pile.size() - 1))) {
-                                    currPlayer.playCard(currPlayer.getHandSize());
-                                    pile.add(chosenDrawnCard);
-
-                                    System.out.println();
-                                    System.out.println("Top Card: " + pile.get(pile.size() - 1).getCard());
-
-                                    if (pile.get(pile.size() - 1) instanceof WildCard &&
-                                            !pile.get(pile.size() - 1).getColor().equals("NONE"))
-                                        System.out.println("Active Color: " +
-                                                ((WildCard) pile.get(pile.size() - 1)).getActiveColor());
-
-                                    chosenDrawnCard.applyCardEffect(this);
-                                } else {
-                                    System.out.println("Invalid option! Card is not valid.");
-                                    System.out.println("Turn skipped!");
-                                    System.out.println();
-                                    System.out.println("Top Card: " + pile.get(pile.size() - 1).getCard());
-
-                                    if (pile.get(pile.size() - 1) instanceof WildCard &&
-                                            !pile.get(pile.size() - 1).getColor().equals("NONE"))
-                                        System.out.println("Active Color: " +
-                                                ((WildCard) pile.get(pile.size() - 1)).getActiveColor());
-                                }
-
-                                break;
-                            case 2:
-                                System.out.println("Turn skipped!");
-                                System.out.println();
-                                System.out.println("Top Card: " + pile.get(pile.size() - 1).getCard());
-
-                                if (pile.get(pile.size() - 1) instanceof WildCard &&
-                                        !pile.get(pile.size() - 1).getColor().equals("NONE"))
-                                    System.out.println("Active Color: " +
-                                            ((WildCard) pile.get(pile.size() - 1)).getActiveColor());
-                                break;
-                        }
-
-                        isTurnOver = true;
-                        System.out.println();
-                        break;
-                }
-            }
-
-            if (currPlayer.hasUno()) {
-                currPlayer.shoutUno();
-                System.out.println();
-            }
-
-            if (currPlayer.isHandEmpty())
-                isGameOver = true;
-            else advanceTurn();
+        switch (choice) {
+            case 1:
+                handlePlayDrawnCard(player);
+                break;
+            case 2:
+                handleKeepDrawnCard(player);
+                break;
         }
 
+        System.out.println();
+        return true;
+    }
+
+    public void handlePlayDrawnCard(Player player) {
+        Card card = player.getCard(player.getHandSize());
+
+        if (card.canPlayOn(getTopCard())) {
+            player.playCard(player.getHandSize());
+            pile.add(card);
+
+            System.out.println();
+            displayTopCard();
+
+            card.applyCardEffect(this);
+        } else {
+            System.out.println("Invalid option! Card is not valid.");
+            System.out.println("Turn skipped!");
+            System.out.println();
+            displayTopCard();
+        }
+    }
+
+    public void handleKeepDrawnCard(Player player) {
+        System.out.println("Turn skipped!");
+        System.out.println();
+        displayTopCard();
+    }
+
+    public void displayTopCard() {
+        Card card = getTopCard();
+        System.out.println("Top Card: " + card.getCard());
+
+        if (getTopCard() instanceof WildCard && !card.getColor().equals("NONE"))
+            System.out.println("Active Color: " + ((WildCard) card).getActiveColor());
+    }
+
+    public void checkIfUno(Player player) {
+        if (player.hasUno()) {
+            player.shoutUno();
+            System.out.println();
+        }
+    }
+
+    public void checkIfGameOver(Player player) {
+        if (player.isHandEmpty())
+            isGameOver = true;
+    }
+
+    public void displayWinner() {
         if (isGameOver) {
             System.out.println(players.get(playerIdx).getName() + " is the winner!");
             System.out.println();
