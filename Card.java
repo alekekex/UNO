1d765cb9d926f76b41e84cec2a6fc3c0a87028ec
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-06 14:32:44 +0800

    changed color attributes to protected

--- Card.java
+++ Card.java
@@ -1,10 +1,10 @@
 public class Card {
-    private static final String RED = "\u001B[31m";
-    private static final String GREEN = "\u001B[32m";
-    private static final String YELLOW = "\u001B[33m";
-    private static final String BLUE = "\u001B[34m";
-    private static final String GRAY = "\u001B[38;5;244m";
-    private static final String RESET = "\u001B[0m";
+    protected static final String RED = "\u001B[31m";
+    protected static final String GREEN = "\u001B[32m";
+    protected static final String YELLOW = "\u001B[33m";
+    protected static final String BLUE = "\u001B[34m";
+    protected static final String GRAY = "\u001B[38;5;244m";
+    protected static final String RESET = "\u001B[0m";
 
     private String value;
     private String color;
